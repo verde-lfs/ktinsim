7f0bb9a6d1bed72e47c23dbaf8e20d2eafa8adc3
verde-lfs <noreply@noreply.com>
2023-01-15 09:39:33 +0300

    Add flag, dashlight enums to OutGaugePacket

--- src/main/kotlin/lfs/ktinsim/packets/outsim/OutGaugePacket.kt
+++ src/main/kotlin/lfs/ktinsim/packets/outsim/OutGaugePacket.kt
@@ -5,13 +5,14 @@ import lfs.ktinsim.getFloatAt
 import lfs.ktinsim.getIntAt
 import lfs.ktinsim.getUIntAt
 import lfs.ktinsim.getUShortAt
+import lfs.ktinsim.packets.Car
 import lfs.ktinsim.packets.Packet
 
 data class OutGaugePacket(
     val time: UInt,
 
     val car: String,
-    val flags: UShort, // TODO change to outgauge flags
+    val flags: List<Flags>,
     val gear: Byte,
     val playerId: UByte,
     val speed: Float,
@@ -21,8 +22,8 @@ data class OutGaugePacket(
     val fuel: Float,
     val oilPressure: Float,
     val oilTemp: Float,
-    val dashLights: UInt,
-    val showLights: UInt,
+    val dashLights: List<DashLight>,
+    val showLights: List<DashLight>,
     val throttle: Float,
     val brake: Float,
     val clutch: Float,
@@ -38,7 +39,7 @@ data class OutGaugePacket(
     constructor(data: ByteArray) : this(
         time = data.getUIntAt(0),
         car = data.getASCIIString(4, 4),
-        flags = data.getUShortAt(8),
+        flags = Flags.getList(data.getUShortAt(8).toInt()),
         gear = data[10],
         playerId = data[11].toUByte(),
         speed = data.getFloatAt(12),
@@ -48,8 +49,8 @@ data class OutGaugePacket(
         fuel = data.getFloatAt(28),
         oilPressure = data.getFloatAt(32),
         oilTemp = data.getFloatAt(36),
-        dashLights = data.getUIntAt(40),
-        showLights = data.getUIntAt(44),
+        dashLights = DashLight.getList(data.getIntAt(40)),
+        showLights = DashLight.getList(data.getIntAt(44)),
         throttle = data.getFloatAt(48),
         brake = data.getFloatAt(52),
         clutch = data.getFloatAt(56),
@@ -58,4 +59,44 @@ data class OutGaugePacket(
         id = data.getIntAt(92)
     )
 
+    enum class Flags(val value: Int) {
+        SHIFT(1),
+        CTRL(2),
+
+        TURBO(8192),
+        KM(16384),
+        BAR(32768);
+
+        companion object {
+            fun getList(data: Int): List<Flags> {
+                return values().filter {
+                    (data and it.value) > 0
+                }
+            }
+        }
+    }
+
+    enum class DashLight(val value: Int) {
+        SHIFT(1),
+        FULLBEAM(2),
+        HANDBRAKE(4),
+        PITSPEED(8),
+        TC(16),
+        SIGNAL_L(32),
+        SIGNAL_R(64),
+        SIGNAL_ANY(128),
+        OILWARN(256),
+        BATTERY(512),
+        ABS(1024),
+        SPARE(2048),
+        NUM(4096);
+
+        companion object {
+            fun getList(data: Int): List<DashLight> {
+                return values().filter {
+                    (data and it.value) > 0
+                }
+            }
+        }
+    }
 }
